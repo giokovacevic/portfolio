eb6f4fac0c14a6bd49ae0e8304f5879ce2a08053
giokovacevic <kovacevico012@gmail.com>
2025-05-16 00:09:38 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+
+function App() {
+
+
+  return (
+    <>
+      <div className={styles.root}><div>Hello World</div></div>
+    </>
+  )
+}
+
+export default App
